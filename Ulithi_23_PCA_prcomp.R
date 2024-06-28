ce28ea914375a2de830f85e7febcfcc940262fd8
Gabby Panayotakis <gmp175@scarletmail.rutgers.edu>
2024-06-28 11:56:38 -0400

    standardized my code
    this is a good PCA plot

--- Ulithi_23_PCA_prcomp.R
+++ Ulithi_23_PCA_prcomp.R
@@ -1,13 +1,17 @@
-name: "Ulithi_23_PCA"
+name: "Ulithi_23_PCA_prcomp"
 project: "metabolomics_Ulithi_23_PCA"
 repository: "metabolomics_Ulithi_23"
 
-#------------------------------------------------------------------------------
+#how to use:
+#load the correct csv file
+#change PCA plot name
+#-------------------------------------------------------------------------------
 
-rm(list=ls())   #clear the environment
+#clear the environment
 
-#------------------------------------------------------------------------------
-#load packages
+rm(list=ls()) 
+
+#load the libraries
 
 library(dplyr)
 library(ggplot2)
@@ -15,140 +19,40 @@ library(tidyverse, quietly = TRUE) #tidyverse-v1.3.1
 library(plotly)
 library(ggfortify)
 
-#------------------------------------------------------------------------------
+#load your CSV files
 
-#read the csv files
-pos_norm_ft2 <- read.csv("ulithi_23_PCA_csv/Ulithi23_pos_norm_ft_6_24.csv", header = TRUE)
-pos_norm_md2 <- read.csv("ulithi_23_PCA_csv/Ulithi23_pos_norm_md_6_24.csv", header = TRUE)
+ft_df <- read.csv("ulithi_23_PCA_csv/Ulithi23_pos_raw_ft_6_24.csv", header = TRUE)
+md_df <- read.csv("ulithi_23_PCA_csv/Ulithi23_pos_raw_md_6_24.csv", header = TRUE)
 
-#------------------------------------------------------------------------------
-#make the PCA
-
-PCA_2 <- prcomp(as.matrix(t(pos_norm_ft2[, -1])),scale = FALSE, center = FALSE )
-
-loadings2 <- PCA_2$loadings #not working
-loadings2
-?loadings
+    #to have the shortened file path, need to set working directory to project directory
+    #session -> set working directory -> to project directory
 
 
-PC_2 <- PCA_2$scores #this is the issue, its null
+#do the principal component analysis
 
-#make the plot
+ft_df <- ft_df[, -1]
+ft_df <- t(ft_df)
 
-df_PC_2 <- data.frame(PC_2)   #needs to be a dataframe so it can be manipulated
+PCA <- prcomp(log(ft_df)); PCA 
+pc.data <- summary(PCA); pc.data
+plot(PCA)
+x_vals <- PCA[["x"]]
 
-final_df <- cbind(df_PC_2, pos_norm_md) #combine the PC values with the metadata
-
-stupid_plot_2 <- ggplot(final_df, aes(Comp.1, Comp.2, colour = genus)) +  #pull genus from the metadata
-    geom_point()  +
-    ggtitle("stupid plot 2"); stupid_plot_2
-
-ggsave(plot = stupid_plot_2, filename = "stupid_plot_2.pdf",units = "in",width = 8.5, height = 8.5) #in indicates inches
-
-#make the plot
-
-
-final_df <- cbind(PC_3, pos_norm_md) #combine the PC values with the metadata
-
-stupid_plot_2 <- ggplot(final_df, aes(Comp.1, Comp.2, colour = genus)) +  #pull genus from the metadata
-    geom_point()  +
-    ggtitle("stupid plot 2"); stupid_plot_2
-
-ggsave(plot = stupid_plot_2, filename = "stupid_plot_2.pdf",units = "in",width = 8.5, height = 8.5) #in indicates inches
-
-#-------------------------------------------------------------------------------
-#try a diff way
-
-pos_norm_ft2 <- pos_norm_ft2[, -1]
-pos_norm_ft2 <- t(pos_norm_ft2)
-
-PCA <- prcomp(pos_norm_ft2)
-
-hm <- PCA[["x"]]
-
-ggplot(hm, aes(PC1, PC2))  +
+ggplot(x_vals, aes(PC1, PC2))  +
     geom_point()
 
-#a better plot
+#make the PCA plot
 
-hm2 <- as.data.frame(hm)
+x_vals <- as.data.frame(x_vals)
 
-final_df <- cbind(hm, pos_norm_md2) #combine the PC values with the metadata
+df_combo <- cbind(x_vals, md_df) #combine the PC values with the metadata
 
-butts_plot <- ggplot(final_df, aes(PC1, PC2, colour = genus)) +  #pull genus from the metadata
+    #change plot name after here
+
+pos_raw_log <- ggplot(df_combo, aes(PC1, PC2, colour = genus)) +  #pull genus from df_combo
     geom_point()  +
-    ggtitle("butts_plot"); butts_plot
+    ggtitle("pos_raw_log"); pos_raw_log
 
-ggsave(plot = butts_plot, filename = "butts_plot.pdf",units = "in",width = 8.5, height = 8.5) #in indicates inches
+ggsave(plot = pos_raw_log, filename = "pos_raw_log.pdf",units = "in",width = 8.5, height = 8.5) #in indicates inches
 
-
-#----------------------------
-#garbage
-
-pos_norm_ft2 <- t(pos_norm_ft2)
-pos_norm_ft2<- as.data.frame(pos_norm_ft2)
-colnames(pos_norm_ft2) <- pos_norm_ft2[1,]
-pos_norm_ft2 <- pos_norm_ft2[-1, ]
-pos_norm_ft2 <- as.numeric(pos_norm_ft2)
-is.numeric(pos_norm_ft2)
-
-rownames(pos_norm_ft2) <- pos_norm_ft2[,1 ]
-
-pos_norm_ft2_flipped <- t(pos_norm_ft2)
-colnames(pos_norm_ft2_flipped) <- pos_norm_ft2_flipped[1, ]
-pos_norm_ft2_flipped <- pos_norm_ft2_flipped[-1, ]
-
-
-numeric <- pos_norm_ft2_flipped[-1, ]
-
-
-PCA <- prcomp(numeric, scale=TRUE)
-?prcomp
-
-hm <- PCA[["x"]]
-
-hm2 <- hm[, 1:2]
-
-ggplot(hm2, aes(PC1, PC2))  +
-    geom_point()
-
-
-
-
-
-
-
-# Only keep rows where 'pts' is less than 10 and 'rebs' is less than 6
-new_df <- subset(df, pts < 10 & rebs < 6)
-
-new_hm <- subset(hm2, PC1 < 2 )
-?subset
-
-
-    PC <- as.data.frame(wine_pca$x)
-    head(PC)
-    
-PC <- as.data.frame(PCA$x)
-is.data.frame(PC)
-
-head(PC)
-
-#plot that works
-
-ggplot(PC, aes(PC1, PC2))   +
-    geom_point()
-
-
-
-#plot
-
-final_df <- cbind(PC, pos_norm_md) #combine the PC values with the metadata
-
-stupid_plot_2 <- ggplot(final_df, aes(Comp.1, Comp.2, colour = genus)) +  #pull genus from the metadata
-    geom_point()  +
-    ggtitle("stupid plot 2"); stupid_plot_2
-
-ggsave(plot = stupid_plot_2, filename = "stupid_plot_2.pdf",units = "in",width = 8.5, height = 8.5) #in indicates inches
-
-
-
+#add scaling
